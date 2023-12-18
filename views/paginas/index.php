02c5390d273d205e8662758ef10ad08d0709e190
Juan Villegas <juancvillefer@gmail.com>
2023-12-18 18:57:47 -0400

    update index.php

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -38,7 +38,7 @@
     <section class="imagen-contacto">
         <h2>Encuentra la casa de tus sueños</h2>
         <p>Llena el formulario de contacto y un asesor se pondrá en contacto contigo a la brevedad</p>
-        <a href="contacto.php" class="boton-amarillo">Contactános</a>
+        <a href="/contacto" class="boton-amarillo">Contactános</a>
     </section>
 
     <div class="contenedor seccion seccion-inferior">
